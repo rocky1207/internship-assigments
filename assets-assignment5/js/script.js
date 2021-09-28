916c550639dd9348612a4a828a999d5a8b031e79
rocky1207 <mjrocky1918@gmail.com>
2021-09-28 22:25:27 +0200

    almost done

--- assets-assignment5/js/script.js
+++ assets-assignment5/js/script.js
@@ -8,18 +8,43 @@ function start() {
         footer[0].removeAttribute('class');
         footer[0].setAttribute('class', 'wrappBig');
     }
-}
-function validateForm(f) {
-    if(f.email.value != '') {
-        var p = document.createElement('p');
-        var form = document.getElementsByTagName('form');
-        
-        p.innerHTML = 'Kralju';
-        p.style.color = '#fff';
-        form[0].appendChild(p);
-        
-       //alert('Kralju');
+    if(w<768) {
+        const form = document.querySelector('.contactTab');
+        const ignore = document.querySelector('.ignore');
+        form.remove();
+        ignore.remove();
     }
-    //return true;
+    const form = document.querySelector('.contact');
+    const input = document.querySelector('.input');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (input.value === '') {
+            document.querySelector('.check').innerHTML = 'Oops! Please add your email';
+            document.querySelector('.check').style.color = '#FB3E3E';
+            if(w<768) {
+                document.querySelector('.check').style.padding = '8px 10%';
+            } else {
+                document.querySelector('.check').style.padding = '8px 0 0 32px';
+            }
+        } else {
+            const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(!input.value.match(pattern)) {
+                document.querySelector('.check').innerHTML = 'Oops! Please check your email';
+                document.querySelector('.check').style.color = '#FB3E3E';
+                if(w<768) {
+                    document.querySelector('.check').style.padding = '8px 10%';
+                } else {
+                    document.querySelector('.check').style.padding = '8px 0 0 32px';
+                }
+            } else {
+                document.querySelector('.check').innerHTML = 'Succeess!!!';
+                document.querySelector('.check').style.color = '#54E6AF';
+                if(w<768) {
+                    document.querySelector('.check').style.padding = '8px 10%';
+                } else {
+                    document.querySelector('.check').style.padding = '8px 0 0 32px';
+                }
+            }
+        }
+    })
 }
-
