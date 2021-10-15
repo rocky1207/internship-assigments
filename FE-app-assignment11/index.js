d00caae1aa772280cc5752794e3f423054077d19
rocky1207 <mjrocky1918@gmail.com>
2021-10-15 13:42:45 +0200

    changes done

--- FE-app-assignment11/index.js
+++ FE-app-assignment11/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const faker = require("faker");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const faker = require('faker');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
 
@@ -10,37 +10,37 @@ app.use(express.urlencoded());
 app.use(express.json());
 // allow CORS
 app.use(
-  cors({
-    origin: "http://127.0.0.1:5500/",
-  })
+    cors({
+        origin: 'http://127.0.0.1:5500/',
+    })
 );
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get('/', (req, res) => {
+    res.send('Hello world');
 });
 
 const users = Array.from({ length: 5 }, (_, i) => ({
-  id: i.toString(),
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  address: faker.address.streetAddress(),
-  country: faker.address.country(),
-  company: faker.company.companyName(),
+    id: i.toString(),
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    address: faker.address.streetAddress(),
+    country: faker.address.country(),
+    company: faker.company.companyName(),
 }));
 
-app.get("/api/users", (req, res) => {
-  res.send(users);
+app.get('/api/users', (req, res) => {
+    res.send(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  res.send(users.find((user) => user.id === req.params.id));
+app.get('/api/users/:id', (req, res) => {
+    res.send(users.find((user) => user.id === req.params.id));
 });
 
-app.post("/api/users", (req, res) => {
-  console.log(req.body);
-  const user = req.body;
-  user.id = users.length;
-  users.push(user);
-  res.send();
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    const user = req.body;
+    user.id = users.length;
+    users.push(user);
+    res.send();
 });
 
 const port = process.env.PORT || 3001;
